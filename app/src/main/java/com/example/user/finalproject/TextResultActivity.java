7cc74d2431b55c82247b1676e8dfa98340053b2c
DuoShiba <dodoguit91095@gmail.com>
2018-11-21 09:00:43 +0800

    APP VERSION1

--- app/src/main/java/com/example/user/finalproject/TextResultActivity.java
+++ app/src/main/java/com/example/user/finalproject/TextResultActivity.java
@@ -5,28 +5,37 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
 import android.media.Image;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
+
+import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
+import java.net.URI;
 import java.net.URL;
+import java.net.URLConnection;
 
 public class TextResultActivity extends AppCompatActivity {
     //TextView t1= (TextView)findViewById (R.id.active);
     int GET_FROM_CAMERA=1;
     int GET_FROM_PICTURE=2;
     ImageView Original;
-    Button alltext,Refresh;
+    Button alltext;
     ImageView Img1,Img2;
     AlertDialog.Builder adb;
     String values=new String();
@@ -37,28 +46,11 @@ public class TextResultActivity extends AppCompatActivity {
     TextView ti1,ti2;
     TextView s1,s2;
     Button si1,si2;
+    Button L1,L2;
+    Uri uri1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-       /* //讀取網路圖片，型態為Bitmap
-
-        private static Bitmap getBitmapFromURL(String imageUrl)
-        {
-            try
-            {
-                URL url = new URL(imageUrl);
-                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-                connection.setDoInput(true);
-                connection.connect();
-                InputStream input = connection.getInputStream();
-                Bitmap bitmap = BitmapFactory.decodeStream(input);
-                return bitmap;
-            }
-            catch (IOException e)
-            {
-                e.printStackTrace();
-                return null;
-            }
-        }*/
 
         super.onCreate(savedInstanceState);
         //t1.setMovementMethod(ScrollingMovementMethod.getInstance());
@@ -78,10 +70,11 @@ public class TextResultActivity extends AppCompatActivity {
         adb=new AlertDialog.Builder(this);
         setContentView(R.layout.activity_text_result);
         alltext =(Button)findViewById(R.id.button);
-//        Refresh=(Button)findViewById(R.id.button2);
-        Img1=(ImageView)findViewById(R.id.imageButton);
-        Img2=(ImageView)findViewById(R.id.imageButton2);
-//        Original=(ImageView)findViewById(R.id.imageView1);
+        L1=(Button)findViewById(R.id.button1);
+        L2=(Button)findViewById(R.id.button2);
+//        Img1=(ImageView)findViewById(R.id.imageButton);
+//        Img2=(ImageView)findViewById(R.id.imageButton2);
+//        Original=(ImageView)findViewById(R.id.imageView);
         tv=(TextView)findViewById(R.id.active);
 
         ti1=(TextView)findViewById(R.id.title1);
@@ -91,8 +84,10 @@ public class TextResultActivity extends AppCompatActivity {
 //        si1=(Button) findViewById(R.id.link1);
 //        si2=(TextView)findViewById(R.id.snippet2);
 
-        Img1.setOnClickListener(GoOUT);
-        Img2.setOnClickListener(I1);
+//        Img1.setOnClickListener(GoOUT);
+//        Img2.setOnClickListener(I1);
+        L1.setOnClickListener(GoOUT);
+        L2.setOnClickListener(GoOUT2);
         alltext.setOnClickListener(S);
 //        Refresh.setOnClickListener(I2);
         tv.setText(values);
@@ -103,8 +98,12 @@ public class TextResultActivity extends AppCompatActivity {
        /* si1.setText("網站連結1");
         si1.setOnClickListener(GoOUT);*/
 //        tv.setText("dsadasdads");
-
+//        Img1.setImageURI(uri1.parse("https://zh.wikipedia.org/wiki/%E6%9F%B4%E7%8A%AC#/media/File:Shiba_Inu.jpg"));
+//        Picasso.with(this).load("https://zh.wikipedia.org/wiki/%E6%9F%B4%E7%8A%AC#/media/File:Shiba_Inu.jpg").into(Original);
+//        new DownloadImageTask((ImageButton)findViewById(R.id.imageButton)).execute("https://zh.wikipedia.org/wiki/%E6%9F%B4%E7%8A%AC#/media/File:Shiba_Inu.jpg");
     }
+
+
     private View.OnClickListener S = new View.OnClickListener() {
 
         @Override
@@ -117,39 +116,6 @@ public class TextResultActivity extends AppCompatActivity {
 
     };
 
-    private View.OnClickListener I1 = new View.OnClickListener() {
-
-        @Override
-        public void onClick(View v) {
-            Intent intents1=new Intent();
-            intents1.setAction(Intent.ACTION_VIEW);
-            intents1.setData(Uri.parse(l2));
-            startActivity(intents1);
-/*         adb.setMessage(DataGetter(R.drawable.camera));
-            adb.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-
-                }
-            });
-            adb.show();*/
-        }
-
-    };
-
-
-    private View.OnClickListener I2 = new View.OnClickListener() {
-
-        @Override
-        public void onClick(View v) {
-            refresh();
-
-        }
-        Intent intent;
-
-    };
-
-
     private View.OnClickListener GoOUT= new View.OnClickListener() {
         @Override
         public void onClick(View v) {
@@ -157,9 +123,18 @@ public class TextResultActivity extends AppCompatActivity {
             intents1.setAction(Intent.ACTION_VIEW);
             intents1.setData(Uri.parse(l1));
             startActivity(intents1);
-
         }
 
+    };
+    private View.OnClickListener GoOUT2 = new View.OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+            Intent intents1=new Intent();
+            intents1.setAction(Intent.ACTION_VIEW);
+            intents1.setData(Uri.parse(l2));
+            startActivity(intents1);
+        }
     };
 
 
@@ -169,6 +144,8 @@ public class TextResultActivity extends AppCompatActivity {
     {
         return "This is not finish yet";
     };
+
+
     @Override
     protected void onActivityResult(int requestCode,int resultCode,Intent intent)
     {
@@ -181,9 +158,57 @@ public class TextResultActivity extends AppCompatActivity {
         super.onResume();
 
     }
-    public void refresh() {
 
-        onCreate(null);
 
+
+        //網址轉圖片測試
+    private Bitmap getImageBitmap(String url) {
+        Bitmap bm = null;
+        try {
+            URL aURL = new URL(url);
+            URLConnection conn = aURL.openConnection();
+            conn.connect();
+            InputStream is = conn.getInputStream();
+            BufferedInputStream bis = new BufferedInputStream(is);
+            bm = BitmapFactory.decodeStream(bis);
+            bis.close();
+            is.close();
+        } catch (IOException e) {
+
+        }
+        return bm;
+    }
+    public static Drawable LoadImageFromWebOperations(String url) {
+        try {
+            InputStream is = (InputStream) new URL(url).getContent();
+            Drawable d = Drawable.createFromStream(is, "src name");
+            return d;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+
+        public DownloadImageTask(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            Bitmap mIcon11 = null;
+            try {
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                mIcon11 = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return mIcon11;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            bmImage.setImageBitmap(result);
+        }
     }
 }
